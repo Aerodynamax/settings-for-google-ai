70fd342ecda13ba9f8d3f4f3f9e22aee56b71d06
Max <maxmillion.bunker@gmail.com>
2025-05-01 20:52:10 +1000

    Initial commit

---
+++ scripts/content.js
@@ -0,0 +1,20 @@
+
+// AI overview size
+document.querySelector(".h7Tj7e").style.setProperty('min-height', '150px', '');
+document.querySelector(".h7Tj7e").style.setProperty('max-height', '150px', '');
+
+// fade
+document.querySelector(".RDmXvc").style.removeProperty('background-image');
+document.querySelector(".RDmXvc").style.setProperty('background', '#101218', '');
+
+document.querySelector(".RDmXvc").style.setProperty('min-height', '80px', '');
+document.querySelector(".RDmXvc").style.setProperty('max-height', '80px', '');
+
+// "Show More" button
+document.querySelector(".clOx1e.sjVJQd>span").textContent = "Show Overview";
+
+document.querySelector(".Kevs9.SLPe5b:has(>div[jscontroller='qTdDb']>#eKIzJc)").className += "s6JM6d ufC5Cb";
+
+document.querySelector(".zNsLfb.Jzkafd").addEventListener('click', () => {
+    document.querySelector(".Kevs9.SLPe5bs6JM6d.ufC5Cb").className = "Kevs9 SLPe5b";
+});
