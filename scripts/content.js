e44fe1f0ec591ea3bea262516c55c51d4f89eb40
Max <maxmillion.bunker@gmail.com>
2025-05-04 22:12:15 +1000

    moved static changes to css

--- scripts/content.js
+++ scripts/content.js
@@ -20,35 +20,17 @@ function waitForElm(selector) {
     });
 }
 
-// "Searching" preview
-waitForElm('.hoqQCc.MyTwIe').then((overview_panel) => {
-
-    // shrink size
-    overview_panel.style.setProperty('height', '48px');
-    
-    // make AI Overview panel be same size as search results
-    document.querySelector(".Kevs9.SLPe5b:has(>div[jscontroller='qTdDb']>#eKIzJc)").className += "s6JM6d ufC5Cb";
-
-});
-
 // "Generating" preview
 waitForElm('[class="hoqQCc MyTwIe"]').then((progress_elem) => {
-
-    // shrink size
-    progress_elem.style.setProperty('height', '48px');
-
-    // make animation less intrusive
-    progress_elem.style.setProperty('opacity', '0.2');
 
     // remove overflowing animation elements
     while ( progress_elem.children[2] ) {
         progress_elem.childNodes.item(2).remove()
     }
-    
 
 });
 
-// wait for the AI overview panel to appear
+// wait for final AI overview panel to appear
 waitForElm('[jsaction="trigger.OiPALb"]').then((btn_showMore) => {
 
     // shrink AI overview panel height
@@ -57,34 +39,44 @@ waitForElm('[jsaction="trigger.OiPALb"]').then((btn_showMore) => {
     overview_panel.style.setProperty('min-height', '150px');
     overview_panel.style.setProperty('max-height', '150px');
 
-    // hide AI text
-    AI_text = document.querySelector(".RJPOee.mNfcNd");
-    AI_text.style.setProperty('display', 'none');
-
-    // fade
-    fade_btn = document.querySelector(".RDmXvc");
-    fade_btn.style.removeProperty('background-image');
-    // fade_btn.style.setProperty('background', '#101218');
-
-    fade_btn.style.setProperty('min-height', '80px');
-    fade_btn.style.setProperty('max-height', '80px');
-    
-    fade_btn.addEventListener('click', onOpen);
-    
     // "Show More" button
     document.querySelector(".clOx1e.sjVJQd>span").textContent = "Show Overview";
     
     // Add openning logic to all "Show More" buttons
     document.querySelectorAll('[jsaction="trigger.OiPALb"]').forEach( btn_elem => btn_elem.addEventListener('click', onOpen) );
     
-
 });
 
 function onOpen() {
     // make AI Overview panel return to default size
-    document.querySelector(".Kevs9.SLPe5bs6JM6d.ufC5Cb").className = "Kevs9 SLPe5b";
 
-    // unhide AI text
-    AI_text = document.querySelector(".RJPOee.mNfcNd");
-    AI_text.style.setProperty('display', 'block');
+    search_size_overview = document.querySelector(".Kevs9.SLPe5bs6JM6d.ufC5Cb")
+    
+    if (search_size_overview) {
+        search_size_overview.className = "Kevs9 SLPe5b";
+    }
 }
+
+
+// center if needed
+waitForElm("#Odp5De:not(:has(.Kevs9.SLPe5b.HdCKGe)) .Kevs9:has( #eKIzJc )").then((overview_container) => {
+
+    waitForElm("#center_col").then((search_results) => {
+
+        overview_container = document.querySelector("#Odp5De:not(:has(.Kevs9.SLPe5b.HdCKGe)) .Kevs9:has( #eKIzJc )");
+
+        if (overview_container != null) { overview_container.className += "s6JM6d ufC5Cb"; }
+    });
+
+    // make AI Overview panel be same size as search results
+    
+    // "searching" often messes with this, delete incorrect styling.Kevs9.SLPe5bs6JM6d.ufC5Cb
+    // document.querySelector("#Odp5De:has(.Kevs9.SLPe5b.HdCKGe) .Kevs9.SLPe5bs6JM6d.ufC5Cb").className = "Kevs9 SLPe5b";
+    // (change the size if there isn't a long snippet at the top)
+    // document.querySelector("#Odp5De:not(:has(.Kevs9.SLPe5b.HdCKGe)) .Kevs9.SLPe5b:has(>div[jscontroller='qTdDb']>#eKIzJc)")
+    
+    // if (document.querySelectorAll("#Odp5De>.YNk70c.EjQTId").length < 3) {
+    //     document.querySelector(".Kevs9.SLPe5b:has(>div[jscontroller='qTdDb']>#eKIzJc)").className += "s6JM6d ufC5Cb";
+    // }
+
+});
