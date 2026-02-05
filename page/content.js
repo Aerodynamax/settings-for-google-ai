300a0e907a046e3643a2f642f4ad2488c5926bea
Max <maxmillion.bunker@gmail.com>
2026-02-05 18:03:41 +1100

    wtf is this codebase?? + fixed settings storage not working + switched popup to traditional extension popup

--- page/content.js
+++ page/content.js
@@ -1,12 +1,11 @@
-
 // https://stackoverflow.com/a/61511955
 function waitForElm(selector) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
         if (document.querySelector(selector)) {
             return resolve(document.querySelector(selector));
         }
 
-        const observer = new MutationObserver(mutations => {
+        const observer = new MutationObserver((mutations) => {
             if (document.querySelector(selector)) {
                 observer.disconnect();
                 resolve(document.querySelector(selector));
@@ -15,57 +14,57 @@ function waitForElm(selector) {
 
         observer.observe(document, {
             childList: true,
-            subtree: true
+            subtree: true,
         });
     });
 }
 
 // "Generating" preview
 waitForElm('[class="hoqQCc MyTwIe"]').then((progress_elem) => {
-
     // remove overflowing animation elements
-    while ( progress_elem.children[2] ) {
-        progress_elem.childNodes.item(2).remove()
+    while (progress_elem.children[2]) {
+        progress_elem.childNodes.item(2).remove();
     }
-
 });
 
 // wait for final AI overview panel to appear
 waitForElm('[jsaction="trigger.OiPALb"]').then((btn_showMore) => {
-
     // shrink AI overview panel height
     overview_panel = document.querySelector(".h7Tj7e");
 
-    overview_panel.style.setProperty('min-height', '150px');
-    overview_panel.style.setProperty('max-height', '150px');
+    overview_panel.style.setProperty("min-height", "150px");
+    overview_panel.style.setProperty("max-height", "150px");
 
     // "Show More" button
     document.querySelector(".clOx1e.sjVJQd>span").textContent = "Show Overview";
-    
+
     // Add openning logic to all "Show More" buttons
-    document.querySelectorAll('[jsaction="trigger.OiPALb"]').forEach( btn_elem => btn_elem.addEventListener('click', onOpen) );
-    
+    document
+        .querySelectorAll('[jsaction="trigger.OiPALb"]')
+        .forEach((btn_elem) => btn_elem.addEventListener("click", onOpen));
 });
 
 function onOpen() {
     // make AI Overview panel return to default size
 
-    search_size_overview = document.querySelector(".Kevs9.SLPe5bs6JM6d.ufC5Cb")
-    
+    search_size_overview = document.querySelector(".Kevs9.SLPe5bs6JM6d.ufC5Cb");
+
     if (search_size_overview) {
         search_size_overview.className = "Kevs9 SLPe5b";
     }
 }
 
-
 // center if needed
-waitForElm("#Odp5De:not(:has( .WC0BKe )) .Kevs9:has( #eKIzJc )").then((overview_container) => {
+waitForElm("#Odp5De:not(:has( .WC0BKe )) .Kevs9:has( #eKIzJc )").then(
+    (overview_container) => {
+        waitForElm("#center_col").then((search_results) => {
+            overview_container = document.querySelector(
+                "#Odp5De:not(:has( .WC0BKe )) .Kevs9:has( #eKIzJc )",
+            );
 
-    waitForElm("#center_col").then((search_results) => {
-
-        overview_container = document.querySelector("#Odp5De:not(:has( .WC0BKe )) .Kevs9:has( #eKIzJc )");
-
-        if (overview_container != null) { overview_container.className += "s6JM6d ufC5Cb"; }
-    });
-    
-});
+            if (overview_container != null) {
+                overview_container.className += "s6JM6d ufC5Cb";
+            }
+        });
+    },
+);
---
+++ popup/script.js
@@ -0,0 +1,26 @@
+// POPUP SETTINGS
+
+(async () => {
+    const optionsForm = document.getElementById("overviewSettings");
+    if (!optionsForm) return;
+
+    // load saved settings
+    const { overviewDisplay = "visible" } =
+        await chrome.storage.local.get("overviewDisplay");
+
+    const radio = optionsForm.querySelector(
+        `input[name="overviewDisplay"][value="${overviewDisplay}"]`,
+    );
+    if (radio) radio.checked = true;
+
+    // save settings changes
+    optionsForm.addEventListener("change", (event) => {
+        var formData = new FormData(optionsForm);
+
+        const options = Object.fromEntries(formData);
+
+        chrome.storage.local.set({
+            overviewDisplay: options.overviewDisplay,
+        });
+    });
+})();
